b5d5f41476ec202d1e0ccf8724548d584dbc7b03
Michael Wood <michael.wood@tocmail.net>
2022-05-09 11:36:03 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-dev-005',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
